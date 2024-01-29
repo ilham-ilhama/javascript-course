9964f1457b4d7f3de383b82a6a0cbf1fa9d0fa39
ilham-ilhama <154922357+ilham-ilhama@users.noreply.github.com>
2024-01-29 15:07:31 +0100

    test git

---
+++ session-01/indek.js
@@ -0,0 +1,54 @@
+//declaration des variables
+/*
+les commentaires multiligne
+*/
+
+
+let nombreUn;
+let nombreDeux;
+let somme;
+
+const PI = 3.14
+
+nombreUn = 16;
+nombreDeux = 20;
+
+somme = nombreUn + nombreDeux;
+
+console.log(somme)
+
+//les fonctions
+
+//procedur
+function greet(nom) {
+    console.log("heloooooo! " + nom)
+}
+
+greet("baddioui")
+
+greet("boukri")
+
+greet("bakarouch")
+
+//fonction
+function sum (x , y) {
+    
+    return x + y
+}
+
+console.log(sum(12, 30))
+
+function inscrire() {
+    let nom = "AAAA";
+    let prenom = "SAMIH";
+    let age = 19;
+
+    if (age == 19) {
+        let statut = "etudiant"
+        console.log(statut)
+    }
+}
+
+inscrire()
+
+
---
+++ session-02/index.js
@@ -0,0 +1,51 @@
+
+/*
+let texte1 = "OFPPT";
+texte1 += " ";
+let texte2 = "enforce";
+let texte3 = texte1 + texte2;
+console.log(texte3)
+
+let = 1+1 ;
+let y = "5" + 1;
+//x=2
+//y="51"
+
+
+a = 10;
+b = '14';
+if (a === b) {
+    console.log("truue")
+}
+else if (a != b) {
+    console.log("true")
+};
+
+
+let statu = "etudiant";
+let age = 20;
+
+if (statu == "etudiant" || age === 20) {
+    console.log("okkkk")
+} else if (statu == "employe") {
+    console.log("error")
+}
+
+let statut = true;
+let age = 20;
+
+if (!statut) {
+    console.log("okkk")
+}
+*/
+let valide = true;
+
+if (!valide) {
+    console.log("ce stagiaire nest pas inscrit")
+}
+
+let test;
+console.log(test)
+if (!test) {
+    console.log("okkk undefined")
+}
