52468c316386e52ead90b0c75d987319e5fb7b51
ilham-ilhama <154922357+ilham-ilhama@users.noreply.github.com>
2024-03-15 14:35:27 +0000

    change the datatype

--- preparation/index.js
+++ preparation/index.js
@@ -63,10 +63,25 @@ let students = 30;
 
 
 
-//change the datatype of value to another 
+//change the datatype of value to another (strings, numbers, booleans)
+// let age = window.prompt("how old are you");
+// age = Number(age);
+// age += 1;
+
+// console.log(age, typeof age);
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
 
 
-
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
 
 
 
